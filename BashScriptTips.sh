227d83e004f48de78cffb530bd6c0350f6a1f73c
Ramos04 <ramos04@ksu.edu>
2019-08-28 13:52:11 -0500

    Updated the tips file

--- BashScriptTips.sh
+++ BashScriptTips.sh
@@ -1,3 +1,20 @@
+     ###################################################################
+     #           ____     ______  ____    __  __                       #
+     #          /\  _`\  /\  _  \/\  _`\ /\ \/\ \                      #
+     #          \ \ \L\ \\ \ \L\ \ \,\L\_\ \ \_\ \                     #
+     #           \ \  _ <'\ \  __ \/_\__ \\ \  _  \                    #
+     #            \ \ \L\ \\ \ \/\ \/\ \L\ \ \ \ \ \                   #
+     #             \ \____/ \ \_\ \_\ `\____\ \_\ \_\                  #
+     #           ____/_____  ___/_____________/_/\____                 #
+     #          /\  _`\ /\ \/\ \/\__  _\ /\  _`\ /\  _`\               #
+     #          \ \ \L\_\ \ \ \ \/_/\ \/ \ \ \/\ \ \ \L\_\             #
+     #           \ \ \L_L\ \ \ \ \ \ \ \  \ \ \ \ \ \  _\L             #
+     #            \ \ \/, \ \ \_\ \ \_\ \__\ \ \_\ \ \ \L\ \           #
+     #             \ \____/\ \_____\/\_____\\ \____/\ \____/           #
+     #              \/___/  \/_____/\/_____/ \/___/  \/___/            #
+     #                                                                 #
+     ###################################################################
+
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
@@ -52,10 +69,132 @@ if [ "$?" != "0" ]; then
   exit 127;
 fi;
 
-# Independent of current working directory
-#-----------------------------------------
+  #-----------------------------------------
+  # Independent of current working directory
+  #-----------------------------------------
 CURR_DIR="$(dirname $0);"
 printf -- 'moving application to /opt/app.jar';
 mv "${CURR_DIR}/application.jar" /opt/app.jar;
 
 
+  #----------------------------------
+  # Check if a script is run as root
+  #----------------------------------
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+  #-----------------
+  # Static variables
+  #-----------------
+readonly tempVar="hello"
+
+##########################
+# DEBUGGING/SYNTAX CHECK
+#################################################################
+
+  #-----------------
+  # Debugging mode
+  #-----------------
+  # Run the script with the following command
+/bin/bash -v <script name>
+
+  #-----------------
+  # Syntax checking
+  #-----------------
+  # Run the script with the following command
+/bin/bash -n <script name>
+
+  #----------------
+  # Script tracing
+  #----------------
+  # Run the script with the following command
+sudo /bin/bash -x <script name>
+
+
+##########################
+# BEST PRACTICIES
+#################################################################
+
+  #-----------
+  # FUNCTIONS
+  #-----------
+  # Don't use the function keyword. All variables created
+  # in a function should be made local.
+
+# WRONG
+function foo {
+    i=foo # this is now global, wrong depending on intent
+}
+
+# RIGHT
+foo() {
+    local i=foo # this is local, preferred
+}
+
+  #------------------
+  # BLOCK STATEMENTS
+  #------------------
+# WRONG
+if true
+then
+    ...
+fi
+
+# RIGHT
+if true; then
+    ...
+fi
+
+  #-----------------
+  # TEST STATEMENTS
+  #-----------------
+  #Use [[ ... ]] for conditional testing, not [ .. ] or test ...
+
+# WRONG
+[ -d /etc ]
+
+# RIGHT
+[[ -d /etc ]]
+
+  #-----------
+  # SEQUENCES
+  #-----------
+  #Use bash builtins for generating sequences
+n=10
+
+# WRONG
+for f in $(seq 1 5); do
+    ...
+done
+
+# WRONG
+for f in $(seq 1 "$n"); do
+    ...
+done
+
+# RIGHT
+for f in {1..5}; do
+    ...
+done
+
+# RIGHT
+for ((i = 0; i < n; i++)); do
+    ...
+done
+
+  #---------------------
+  # COMMAND SUBSTITUION
+  #----------------------
+  #Use $(...) for command substitution.
+
+# WRONG
+foo=`date`
+
+# RIGHT
+foo=$(date)
+
+  #---------------------
+  # COMMAND SUBSTITUION
+  #----------------------
---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo hello
+sleep 3
+echo ummmm
+sleep 5
+temp=5
+if [[ $temp == 5 ]] then
+	echo true
+fi
+echo done
+exit 0
